e54e62b326687945fd8efbee3182362e602edf9d
hartkemd <hartkemd@gmail.com>
2022-01-04 21:06:27 -0500

    Started adding Data Access and added DataGrid control to Window.

---
+++ TextCopierApp/DataAccessLibrary/Models/TextItemModel.cs
@@ -0,0 +1,9 @@
+﻿
+namespace DataAccessLibrary.Models
+{
+    public class TextItemModel
+    {
+        public string Description { get; set; }
+        public string Text { get; set; }
+    }
+}
--- TextCopierApp/TextCopier/MainWindow.xaml.cs
+++ TextCopierApp/TextCopier/MainWindow.xaml.cs
@@ -1,17 +1,10 @@
-﻿using System;
+﻿using DataAccessLibrary;
+using DataAccessLibrary.Models;
+using Microsoft.Extensions.Configuration;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace TextCopier
 {
@@ -20,9 +13,67 @@ namespace TextCopier
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static IConfiguration _config;
+        private static string _filePath;
+        private static TextFileDataAccess db = new();
+        private BindingList<TextItemModel> textItems = new();
+
         public MainWindow()
         {
             InitializeComponent();
+            InitializeConfiguration();
+            _filePath = _config.GetValue<string>("FilePath");
+
+            TextItemModel textItem1 = new() { Description = "Test", Text = "Some text to copy." };
+            textItems.Add(textItem1);
+            textItemsDataGrid.ItemsSource = textItems;
+        }
+
+        private static void GetAllTextItems()
+        {
+            var textItems = db.ReadAllRecords(_filePath);
+
+            foreach (var textItem in textItems)
+            {
+                //$"{textItem.Description} {textItem.Text}";
+            }
+        }
+
+        private static void CreateTextItem(TextItemModel textItem)
+        {
+            var textItems = db.ReadAllRecords(_filePath);
+            textItems.Add(textItem);
+            db.WriteAllRecords(textItems, _filePath);
+        }
+
+        private static void UpdateFirstTextItemsDescription(string description)
+        {
+            var textItems = db.ReadAllRecords(_filePath);
+            textItems[0].Description = description;
+            db.WriteAllRecords(textItems, _filePath);
+        }
+
+        private static void RemoveDescriptionFromTextItem()
+        {
+            var textItems = db.ReadAllRecords(_filePath);
+            textItems[0].Description = "";
+            db.WriteAllRecords(textItems, _filePath);
+        }
+
+        private static void RemoveTextItem()
+        {
+            var textItems = db.ReadAllRecords(_filePath);
+            textItems.RemoveAt(0);
+            db.WriteAllRecords(textItems, _filePath);
+        }
+
+        private static void InitializeConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            _config = builder.Build();
         }
     }
 }
