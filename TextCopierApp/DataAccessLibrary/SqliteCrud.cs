34960d0dc8be98f069039e9db4eca1c8f1ae88a2
hartkemd <hartkemd@gmail.com>
2022-01-10 21:57:21 -0500

    Started work on Sqlite data access classes.

---
+++ TextCopierApp/DataAccessLibrary/SqliteCrud.cs
@@ -0,0 +1,45 @@
+﻿using DataAccessLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary
+{
+    public class SqliteCrud
+    {
+        private readonly string _connectionString;
+        private SqliteDataAccess db = new();
+
+        public SqliteCrud(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public List<TextItemModel> GetAllTextItems()
+        {
+            string sql = "select Id, Description, Text from TextItems;";
+
+            return db.LoadData<TextItemModel, dynamic>(sql, new { }, _connectionString);
+        }
+
+        public void CreateTextItem(TextItemModel textItem)
+        {
+            string sql = "insert into TextItems (Description, Text) values (@Description, @Text);";
+            db.SaveData(sql, new { textItem.Description, textItem.Text }, _connectionString);
+        }
+
+        public void UpdateTextItem(TextItemModel textItem)
+        {
+            string sql = "update TextItems set Description = @Description, Text = @Text where Id = @Id;";
+            db.SaveData(sql, textItem, _connectionString);
+        }
+
+        public void DeleteTextItem(int textItemId)
+        {
+            string sql = "delete from TextItems where Id = @Id;";
+            db.SaveData(sql, new { Id = textItemId }, _connectionString);
+        }
+    }
+}
---
+++ TextCopierApp/DataAccessLibrary/SqliteDataAccess.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Data.SQLite;
+using Dapper;
+using DataAccessLibrary.Models;
+
+namespace DataAccessLibrary
+{
+    public class SqliteDataAccess : IDataAccess
+    {
+        public List<T> LoadData<T, U>(string sqlStatement, U parameters, string connectionString)
+        {
+            using (IDbConnection connection = new SQLiteConnection(connectionString))
+            {
+                List<T> rows = connection.Query<T>(sqlStatement, parameters).ToList();
+                return rows;
+            }
+        }
+
+        public void SaveData<T>(string sqlStatement, T parameters, string connectionString)
+        {
+            using (IDbConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Execute(sqlStatement, parameters);
+            }
+        }
+
+        public List<TextItemModel> ReadAllRecords(string filePath)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void WriteAllRecords(IEnumerable<TextItemModel> textItems, string filePath)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
